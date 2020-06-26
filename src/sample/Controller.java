1818b3183e53193112d075f857e983e4e4733182
vladimir_lysenkov <vladimirv.lysenkov@gmail.com>
2020-06-26 09:38:49 +0500

    ДЗ№4

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    public void sendMassage() {
+        try {
+            textArea.appendText(textField.getText()+"\n");
+            textField.clear();
+            textField.requestFocus();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
